4cfb9636b67b35ccbd375785d524cc394b083942
vitaly <hahabuntun@gmail.com>
2026-01-26 23:07:23 +0300

    update example2

--- examples/example2.py
+++ examples/example2.py
@@ -1,15 +1,28 @@
+"""Capture video and save it to file"""
 import cv2
 
 cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter.fourcc('X', 'V', 'I', 'D')
+out = cv2.VideoWriter("../output/example2.avi", fourcc, 20, (640, 480))
 
-while True:
+
+while cap.isOpened():
     ret, frame = cap.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('frame', gray)
+    if ret:
+        print(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) # frame width
+        print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) # frame heigh
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+        out.write(frame)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('frame', gray)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
         break
 
 cap.release()
+out.release()
 cv2.destroyAllWindows()
