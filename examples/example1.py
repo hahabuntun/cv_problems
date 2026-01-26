10967622cdbd0e7f05ad647a37cd096976f859ae
vitaly <hahabuntun@gmail.com>
2026-01-26 22:55:00 +0300

    add example 2

--- examples/example1.py
+++ examples/example1.py
@@ -1,10 +1,15 @@
+"""
+Read an image from a file and write an image to a file
+"""
 import cv2
 
 img = cv2.imread('../images/lena.jpg', 0)
 
-
 cv2.imshow('image', img)
-cv2.waitKey(5000)
-cv2.destroyAllWindows()
 
-cv2.imwrite('lena_copy.png', img)
+key = cv2.waitKey(0)
+if key == 27:
+    cv2.destroyAllWindows()
+elif key == ord('s'):
+    cv2.imwrite('lena_copy.png', img)
+    cv2.destroyAllWindows()
