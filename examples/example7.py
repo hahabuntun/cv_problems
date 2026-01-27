a089e3296b8efe503d4e2459ceddf85e02b31b5e
vitaly <hahabuntun@gmail.com>
2026-01-27 14:03:23 +0300

    add example 7

---
+++ examples/example7.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+
+
+def click_event(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        # cv2.circle(img, (x, y), 3, (0, 0, 225), -1)
+        # points.append((x, y))
+        # if len(points) >= 2:
+            # cv2.line(img,  points[-1], points[-2], (255, 0, 0), 5)
+        blue = img[x, y, 0]
+        green = img[x, y, 1]
+        red = img[x, y, 2]
+        
+        cv2.circle(img, (x, y), 3, (0, 0, 255), -1)
+        coloredImage = np.zeros((512, 512, 3), np.uint8)
+
+        coloredImage[:] = [blue, green, red]
+        cv2.imshow('color', coloredImage)
+
+
+img = cv2.imread('../images/lena.jpg')
+cv2.imshow('image', img)
+
+points = []
+cv2.setMouseCallback('image', click_event)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
